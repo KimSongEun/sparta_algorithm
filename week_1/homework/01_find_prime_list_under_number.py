b3f04e4be30fd9a1b774006ffc44f94704328de6
KimSongEun <songeun4939@naver.com>
2021-12-27 06:42:54 +0000

    알고리즘_소수나열하기_개션 2번

---
+++ week_1/homework/01_find_prime_list_under_number.py
@@ -0,0 +1,24 @@
+input = 20
+
+# 소수는 자기 자신과 1외에는 아무것도 나눌 수 없다.
+# 주어진 자연수 N이 소수이기 위한 필요 충분 조건은
+# N이 N의 제곱근보다 크지 않은 어떤 소수로도 나눠지지 않는다.
+# 수가 수를 나누면 몫이 발생하는데, 몫과 나누는 수 둘 중 하나는
+# 반드시 N의 제곱근 이하
+def find_prime_list_under_number(number):
+    prime_list = []
+    for n in range(2, number+1): # n의 범위 : 2부터 number까지
+        # n=19
+        # i = 2, 3, 5, 7 .....
+        # 2 -> X / 3 -> X / 6 -> X
+        for i in prime_list: # i의 범위 : 2부터 n-1까지의 소수
+            if n % i == 0 and i * i <= n:
+                break
+        else:
+            prime_list.append(n)
+
+    return prime_list
+
+
+result = find_prime_list_under_number(input)
+print(result)
